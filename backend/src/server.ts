70cb1f60449914b9fcf253434328cc28d0edf7c6
Hassan Lasheen <106620966+Hassanlasheenn@users.noreply.github.com>
2024-10-01 14:53:50 +0300

    add properties for cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,18 +8,12 @@ const port = process.env.PORT || 8080;
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({
         credentials: true,
-        origin: "http://localhost:4200"
+        origin: "http://localhost:4200",
+        methods: 'GET, POST, PUT, DELETE',
+        allowedHeaders: 'Content-Type, Authorization'
     }));
 }
 
-function setCorsHeaders(req: Request, res: any, next: any) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-}
-
-app.use(setCorsHeaders);
 app.use(express.json());
 app.use('/api/foods', foodRoutes);
 
